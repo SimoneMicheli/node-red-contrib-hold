a602f1eb64ac3b4d854937bba2922f22f2b3c120
Simone Micheli <s.micheli965@gmail.com>
2024-10-03 16:40:43 +0200

    add status label

--- hold/hold.js
+++ hold/hold.js
@@ -5,14 +5,18 @@ module.exports = function(RED) {
         var node = this;
         var msg = {};
         node.context().storedmessage = node.context().storedmessage || null;
+	node.status({"shape":"dot","fill":"grey","text":"No message in buffer"})
         this.on('input', function (msg) {
             if ((msg.trigger==null)||(msg.trigger!=true)) {
               node.context().storedmessage = msg;
+	      node.status({"shape":"dot","fill":"blue","text":"Message hold"})
             } else {
               if(node.context().storedmessage!=null) {
                 node.send(node.context().storedmessage);
+		node.status({"shape":"dot","fill":"green","text":"Message triggered"})
               } else {
                 node.warn("Hold '"+n.name+"' triggered without message in buffer.");
+		node.status({"shape":"dot","fill":"yellow","text":"Triggered without message in buffer"})
               }
             }
         });
